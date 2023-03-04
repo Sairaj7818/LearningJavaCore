9cc3d6e71fa623fdaa8f126f02618e2af9f32adb
Sairaj-Tamboskar-Kilowott <92842383+Sairaj-Tamboskar-Kilowott@users.noreply.github.com>
2023-03-04 23:50:32 +0530

    Encapsulation commit

---
+++ Encapsulation.java
@@ -0,0 +1,24 @@
+public class Encapsulation
+{
+    public static void main( String args[])
+    {
+        Human obj = new Human();
+
+        System.out.println(obj.getName() + " : " + obj.getAge());
+    }
+}
+
+class Human
+{ 
+    private int age = 99;  //THIS IS HOW WE MAKE A VARIAL PRIVATE FOR A CLASS. FROM NOW ONWARDS WE WILL DEFINE INSTANCE VARIABLE AS PRIVATE
+    private String name = "Srj"; 
+
+    public String getName()  //METHOD FROMAT: TYPE | RETURN TYPE | NMETHODNAME
+    {
+        return name;
+    }
+     public int getAge()
+     {
+        return age;
+     }
+}
