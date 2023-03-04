1801ad674b5c5f7478a8a9a011ee723a31611ae7
Sairaj-Tamboskar-Kilowott <92842383+Sairaj-Tamboskar-Kilowott@users.noreply.github.com>
2023-03-04 20:37:17 +0530

    StaticMethod Example

--- StaticMethod.java
+++ StaticMethod.java
@@ -1,13 +1,25 @@
-public class Constructor
+public class StaticMethod
 {
-        public static void main (String args[])
+    public static void main (String args[])
     {
         Mobile obj1 = new Mobile();
         obj1.brand = "Apple";
         obj1.price = 2000;
-        //obj1.name = "iPhone";
+        obj1.name = "iPhone";
 
         Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 3000;
+        obj2.name = "Galaxy";
+
+        Mobile.name = "Smartphone";
+
+        obj1.show(); //WE NEED OBJECT TO CALL A INSTANCE METHOD
+        obj2.show();
+
+        Mobile.show1(); //THIS WILL WORK AS WE ARE CALLING STATIC METHOD USING CLASS NAME
+
+        //Mobile.show(); //THIS WILL NOT WORK AS WE CANNOT CALL INSTANCE METHOD USING CLASS NAME
     }
 
 }
@@ -26,15 +38,15 @@ class Mobile
         System.out.println("In static block.");
     }
 
-    public Mobile()  //CONSTRUCTOR DEFINED
-    {   
-        brand = "";
-        price = 200;
-        System.out.println("In the constructor.");
-    }
-
-    public void show()
+    public void show() //INSTANCE METHOD
     {
         System.out.println(brand + " : " + price + " : " + name);
     }
+
+    public static void show1() //STATIC METHOD
+    {
+        System.out.println("In the Static Method.");
+    }
+     
+    
 }
