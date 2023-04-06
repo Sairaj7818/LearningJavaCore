6b60870bcbe7a235d2f02ad583a5f1079b07333a
Sairaj-Tamboskar-Kilowott <92842383+Sairaj-Tamboskar-Kilowott@users.noreply.github.com>
2023-04-06 21:16:44 +0530

    multilevel inheritance

---
+++ InheritVeryAdvCalc.java
@@ -0,0 +1,8 @@
+//THIS IS MULTI LEVEL INHERITANCE WHERE THIS CLASS IS INHERITING PREVIOUS CLASS AND ALL THE FEATURES OF MAIN PARENT CLASS IS ALSO ACCESSIBLE HERE IE. ADDITION AND SUBTRACTIO
+public class InheritVeryAdvCalc extends InheritAdvCalc
+{
+    public double power(int n1, int n2)
+    {
+        return Math.pow(n1,n2);
+    }
+}
--- Inheritance.java
+++ Inheritance.java
@@ -3,16 +3,19 @@ public class Inheritance
 {
     public static void main( String args[])
     {
-        InheritAdvCalc obj = new InheritAdvCalc();
+        // InheritAdvCalc obj = new InheritAdvCalc();
+        InheritVeryAdvCalc obj = new InheritVeryAdvCalc();
         int r1 = obj.add(5,5);
         int r2 = obj.sub(5,5);
         int r3 = obj.multi(5,5);
         int r4 = obj.div(5,5);
+        double r5 = obj.power(4,2);
 
         System.out.println( "additon is:" + r1 );
         System.out.println( "subtraction is:" + r2 );
         System.out.println( "multiplication is:" + r3 );
         System.out.println( "division is:" + r4 );
+        System.out.println("power multiplication is:" + r5);
 
     }
 }
