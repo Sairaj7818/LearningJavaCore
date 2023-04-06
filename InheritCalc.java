711494e2cd7c00f4a0a6f1460461303b9842c66c
Sairaj-Tamboskar-Kilowott <92842383+Sairaj-Tamboskar-Kilowott@users.noreply.github.com>
2023-04-06 21:05:22 +0530

    Inheritance concept 6-4-23

---
+++ InheritCalc.java
@@ -0,0 +1,12 @@
+//THIS IS PARENT CLASS OF CALCULATOR HAVING ADDITION AND SUBTRACTION
+public class InheritCalc
+{
+    public int add(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+    public int sub(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+}
---
+++ Inheritance.java
@@ -0,0 +1,18 @@
+// THIS IS MAIN CLASS AND THIS TASK WILL HAVE 2 MORE FILES 1. InheritCalc 2. InheritAdvCalc
+public class Inheritance
+{
+    public static void main( String args[])
+    {
+        InheritAdvCalc obj = new InheritAdvCalc();
+        int r1 = obj.add(5,5);
+        int r2 = obj.sub(5,5);
+        int r3 = obj.multi(5,5);
+        int r4 = obj.div(5,5);
+
+        System.out.println( "additon is:" + r1 );
+        System.out.println( "subtraction is:" + r2 );
+        System.out.println( "multiplication is:" + r3 );
+        System.out.println( "division is:" + r4 );
+
+    }
+}
