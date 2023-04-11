4b4427eeeb1b7da71c6ed4506f9ac27e0b01cff1
Sairaj-Tamboskar-Kilowott <92842383+Sairaj-Tamboskar-Kilowott@users.noreply.github.com>
2023-04-11 15:33:51 +0530

    Abstract Keyword in OOPs

---
+++ AbstractKeyword.java
@@ -0,0 +1,30 @@
+//ABSTRACT KEYWORD WHICH IS USED TO DECLARE A METHOD AND THIS METHOD CAN BE LATER DEFINED IN ANOTHER CLASS
+
+public class AbstractKeyword
+{
+    public static void main(String args[])
+    {
+        //Car obj = new Car(); //THIS WILL NOT WORK AS WE CANNOT CREATE OBJ OF ABSTRACT CLASS
+        Car obj = new WagonR();      
+        obj.drive();
+        obj.playMusic();
+    }
+}
+
+abstract class Car  //WE NEED TO MAKE CLASS AS ABSTRACT TO HOLD ABSTRACT METHOD
+{
+    public abstract void drive(); //DECLARING A METHOD 
+
+    public void playMusic()
+    {
+        System.out.println("playing music...");
+    }
+}
+
+class WagonR extends Car
+{
+    public void drive() //DEFINING ABOVE DECLARED METHOD
+    {
+        System.out.println("driving...");
+    }
+}
